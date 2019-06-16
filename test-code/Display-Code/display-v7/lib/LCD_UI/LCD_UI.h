afb7bb4eac66612909abc8b8389c5dd07d7637d2
Kevin Kuney <Kevin.Kuney@gmail.com>
2019-06-15 22:50:26 -0500

    Interface is workinggit add *git add *

--- test-code/Display-Code/display-v7/lib/LCD_UI/LCD_UI.h
+++ test-code/Display-Code/display-v7/lib/LCD_UI/LCD_UI.h
@@ -6,24 +6,17 @@
 class LCD_UI
 {
   public:
-    LCD_UI(char **optionsList, int N, int enterPin, int upPin, int downPin, LiquidCrystal &lcd);
-    int update();
-    int enter();
+    LCD_UI(char **optionsList, int N, LiquidCrystal *lcd);
+    int getIndex();
     void up();
     void down();
     void print();
-    void reset();
   private:
     char **_optionsList;
-    int _enterPin;
-    int _upPin;
-    int _downPin;
     int _i;
     int _j;
-    int _k;
     int _N;
-    LiquidCrystal &_lcd;
-    // _States state;
+    LiquidCrystal *_lcd;
 };
 
 #endif
--- test-code/Display-Code/display-v7/src/display-v7.cpp
+++ test-code/Display-Code/display-v7/src/display-v7.cpp
@@ -1,6 +1,6 @@
 // Works with physical push buttons
 
-// #include "LCD_UI.h"
+#include "LCD_UI.h"
 #include "Arduino.h"
 #include <LiquidCrystal.h>
 #include "digital_input.h"
@@ -12,17 +12,17 @@ int downPin = 9;
 unsigned long globalCurrentTime = 0;
 
 enum States {MAIN_MENU, CALIBRATE, SWING_UP, BALANCE};
-//char *options[] = {"Swingup", "Balance", "Calibrate"};
-// States stateOptionArray[] = {SWING_UP, BALANCE, CALIBRATE};
+char *options[] = {"Swing up", "Balance", "Calibrate"};
+States stateOptionArray[] = {SWING_UP, BALANCE, CALIBRATE};
 States state = MAIN_MENU;
+States lastState = CALIBRATE; // Make sure lastState != state as initial value
+States nextState = MAIN_MENU;
 const unsigned long debounceTime = 50000;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 digital_input enterPB(enterPin, debounceTime, debounceTime, &globalCurrentTime, INPUT_PULLUP);
 digital_input upPB(upPin, debounceTime, debounceTime, &globalCurrentTime, INPUT_PULLUP);
-//digital_input downPB(downPin, debounceTime, debounceTime, &globalCurrentTime, INPUT_PULLUP);
-digital_input* downPB;
-// LiquidCrystal *lcd_pointer = &lcd;
-// LCD_UI ui(options,3,13,10,9,lcd);
+digital_input downPB(downPin, debounceTime, debounceTime, &globalCurrentTime, INPUT_PULLUP);
+LCD_UI ui(options, 3, &lcd);
 
 States main_menu(void);
 States calibrate(void);
@@ -31,55 +31,64 @@ States swing_up(void);
 
 void setup() {
     lcd.begin(16,2);
-    downPB = new digital_input(downPin, debounceTime, debounceTime, &globalCurrentTime, INPUT_PULLUP);
-    //lcd.print("Setup");
-    // pinMode(enterPin,INPUT_PULLUP);
+    pinMode(enterPin,INPUT_PULLUP);
     pinMode(upPin,INPUT_PULLUP);
-    //pinMode(downPin,INPUT_PULLUP);
+    pinMode(downPin,INPUT_PULLUP);
 }
 
 void loop() {
     globalCurrentTime = micros();
     switch (state) {
-        case MAIN_MENU: state = main_menu(); break;
-        case CALIBRATE: state = calibrate(); break;
-        case BALANCE:   state = balance();   break;
-        case SWING_UP:  state = swing_up();  break;
+        case MAIN_MENU: nextState = main_menu(); break;
+        case CALIBRATE: nextState = calibrate(); break;
+        case BALANCE:   nextState = balance();   break;
+        case SWING_UP:  nextState = swing_up();  break;
         default: break;
     }
+    lastState = state;
+    state = nextState;
 }
 
 States main_menu() {
+    if (lastState != state) {
+        lcd.clear();
+        ui.print();
+    }
     enterPB.update();
     upPB.update();
-    downPB->update();
-    if (enterPB._risingEdge){
-    	lcd.clear();
-    	lcd.print("Enter");
-    }
-    if (downPB->_risingEdge){
-    	lcd.clear();
-    	lcd.print("Down");
-    }
-    if (upPB._risingEdge){
-    	lcd.clear();
-    	lcd.print("Up");
-    }
+    downPB.update();
+    if (enterPB._risingEdge) return stateOptionArray[ui.getIndex()];
+    if (downPB._risingEdge) ui.down();
+    if (upPB._risingEdge) ui.up();
     return MAIN_MENU;
 }
 
 States balance() {
-	Serial.println("BALANCE STATE");
-	return MAIN_MENU;
+    if (lastState != state) {
+        lcd.clear();
+        lcd.print("BALANCE STATE");
+    }
+    enterPB.update();
+    if (enterPB._risingEdge) return MAIN_MENU;
+	return BALANCE;
 }
 
 States calibrate() {
-	lcd.clear();
-	lcd.print("CALIBRATE");
-	return MAIN_MENU;
+    if (lastState != state) {
+        lcd.clear();
+        lcd.print("CALIBRATE");
+    }
+    enterPB.update();
+    if (enterPB._risingEdge) return MAIN_MENU;
+	return CALIBRATE;
 }
 
 States swing_up() {
-	Serial.println("SWING_UP STATE");
-	return MAIN_MENU;
+    if (lastState != state) {
+        lcd.clear();
+        lcd.print("SWING_UP STATE");
+    }
+    enterPB.update();
+    if (enterPB._risingEdge) return MAIN_MENU;
+	return SWING_UP;
 }
