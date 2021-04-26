6b689a7958195fc7dba34ae6f818e5ee35f79146
Kevin Kuney <Kevin.Kuney@gmail.com>
2021-04-25 22:23:08 -0500

    drawing of assembly is working

--- hmi/src/App.js
+++ hmi/src/App.js
@@ -50,10 +50,10 @@ function Assembly(canvasWidth, canvasHeight) {
     FEET_HEIGHT: 7.95,
 
     // MARGIN
-    MARGIN: 40,
+    MARGIN: 5,
   };
 
-  this.widthMM =
+  this.totalWidth =
     2 *
       (dims.MARGIN +
         dims.PENDULUM_HEIGHT -
@@ -62,20 +62,33 @@ function Assembly(canvasWidth, canvasHeight) {
     dims.LINEAR_SHAFT_LENGTH;
 
   this.heightMM =
-    2 * (dims.MARGIN + dims.PENDULUM_HEIGHT - dims.PENDULUM_HOLE_LOCATION) +
-    dims.LINEAR_SHAFT_LENGTH;
+    2 * (dims.MARGIN + dims.PENDULUM_HEIGHT - dims.PENDULUM_HOLE_LOCATION);
 
   // this.determineScaling = function (canvasWidth, canvasHeight) {
-  let pxmmWidth = canvasWidth / this.widthMM; // pxmm = pixels per mm
+  let pxmmWidth = canvasWidth / this.totalWidth; // pxmm = pixels per mm
   let pxmmHeight = canvasHeight / this.heightMM;
   this.pxmm = pxmmWidth >= pxmmHeight ? pxmmHeight : pxmmWidth;
-  this.widthPx = this.pxmm * this.widthMM;
+  this.widthPx = this.pxmm * this.totalWidth;
   this.heightPx = this.pxmm * this.heightMM;
   // };
+
+  // Fills a rectangle using mm dimensions
+  this.fillRect = function (ctx, x, y, w, h) {
+    ctx.fillRect(this.pxmm * x, this.pxmm * y, this.pxmm * w, this.pxmm * h);
+  };
 
   this.drawCart = function (ctx) {
     let x = 0;
     ctx.fillStyle = "Silver";
+    // ctx.fillRect(
+    //   this.widthPx / 2 + this.pxmm * (x - dims.CART_MOUNT_PLATE_WIDTH / 2),
+    //   this.heightPx / 2 - this.pxmm * dims.ROTARY_BEARING_MOUNT_HOLE_LOCATION,
+    //   this.pxmm * dims.CART_MOUNT_PLATE_WIDTH,
+    //   this.pxmm *
+    //     (dims.LINEAR_BEARING_HEIGHT +
+    //       dims.CART_MOUNT_PLATE_HEIGHT +
+    //       dims.ROTARY_BEARING_MOUNT_HEIGHT)
+    // );
     ctx.fillRect(
       this.widthPx / 2 + this.pxmm * (x - dims.CART_MOUNT_PLATE_WIDTH / 2),
       this.heightPx / 2 - this.pxmm * dims.ROTARY_BEARING_MOUNT_HOLE_LOCATION,
@@ -155,106 +168,9 @@ function Assembly(canvasWidth, canvasHeight) {
     this.drawCart(ctx);
     this.drawPendulum(ctx);
   };
-
-  //   this.draw = function (ctx) {
-  //     ctx.fillStyle = "DarkGrey";
-  //     ctx.fillRect(
-  //       this.state.x - this.state.w / 2,
-  //       this.state.y - this.state.h / 2,
-  //       this.state.w,
-  //       this.state.h
-  //     );
-  //   };
 }
 
-// function Cart({ x, y, w, h }) {
-//   this.state = { x: x, y: y, w: w, h: h };
-//   this.draw = function (ctx) {
-//     ctx.fillStyle = "silver";
-//     ctx.fillRect(
-//       this.state.x - this.state.w / 2,
-//       this.state.y - this.state.h / 2,
-//       this.state.w,
-//       this.state.h
-//     );
-//   };
-//   this.update = function (ctx, { x, y, w, h }) {
-//     this.state = { x: x, y: y, w: w, h: h };
-//     this.draw(ctx);
-//   };
-// }
-
-// function Pendulum({ x, y, theta, w, h }) {
-//   this.state = { x: x, y: y, theta: theta, w: w, h: h };
-//   this.draw = function (ctx) {
-//     ctx.fillStyle = "green";
-//     ctx.translate(this.state.x, this.state.y);
-//     ctx.rotate(this.state.theta + Math.PI);
-//     ctx.translate(-this.state.x, -this.state.y);
-//     ctx.fillRect(
-//       this.state.x - this.state.w / 2,
-//       this.state.y - this.state.h / 20,
-//       this.state.w,
-//       this.state.h
-//     );
-//   };
-//   this.update = function (ctx, { x, y, theta, w, h }) {
-//     this.state = { x: x, y: y, theta: theta, w: w, h: h };
-//     this.draw(ctx);
-//   };
-// }
-
-// function Rail({ x, y, w, h }) {
-//   this.state = { x: x, y: y, w: w, h: h };
-//   this.draw = function (ctx) {
-//     ctx.fillStyle = "DarkGrey";
-//     ctx.fillRect(
-//       this.state.x - this.state.w / 2,
-//       this.state.y - this.state.h / 2,
-//       this.state.w,
-//       this.state.h
-//     );
-//   };
-//   this.update = function (ctx, { x, y, theta, w, h }) {
-//     this.state = { x: x, y: y, theta: theta, w: w, h: h };
-//     this.draw(ctx);
-//   };
-// }
-
-// function Block({ x, y, w, h }) {
-//   this.state = { x: x, y: y, w: w, h: h };
-//   this.draw = function (ctx) {
-//     ctx.fillStyle = "SteelBlue";
-//     ctx.fillRect(
-//       this.state.x - this.state.w / 2,
-//       this.state.y - this.state.h / 2,
-//       this.state.w,
-//       this.state.h
-//     );
-//   };
-//   this.update = function (ctx, { x, y, theta, w, h }) {
-//     this.state = { x: x, y: y, theta: theta, w: w, h: h };
-//     this.draw(ctx);
-//   };
-// }
-
 function App() {
-  // var cart = new Cart({ x: 0, y: 200, w: 100, h: 50 });
-  // var pendulum = new Pendulum({ x: 0, y: 200, theta: 0, w: 20, h: 200 });
-  // var rail = new Rail({ x: window.innerWidth / 2, y: 200, w: 900, h: 20 });
-  // var leftBlock = new Block({
-  //   x: window.innerWidth / 2 - rail.state.w / 2 - 25,
-  //   y: 200 + 10,
-  //   w: 50,
-  //   h: 60,
-  // });
-  // var rightBlock = new Block({
-  //   x: window.innerWidth / 2 + rail.state.w / 2 + 25,
-  //   y: 200 + 10,
-  //   w: 50,
-  //   h: 60,
-  // });
-
   var assembly = new Assembly(window.innerWidth, window.innerHeight);
   console.log(window.innerWidth, window.innerHeight);
   const draw = (ctx, frameCount) => {
@@ -263,19 +179,10 @@ function App() {
 
     ctx.canvas.width = assembly.widthPx;
     ctx.canvas.height = assembly.heightPx;
-    // console.log(assembly.widthMM, assembly.heightMM);
+    console.log(assembly.widthMM, assembly.heightMM);
     // let frameCountMod = frameCount % ctx.canvas.width;
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     assembly.draw(ctx);
-    // rail.update(ctx, rail.state);
-    // leftBlock.update(ctx, leftBlock.state);
-    // rightBlock.update(ctx, rightBlock.state);
-    // cart.update(ctx, { ...cart.state, x: frameCountMod });
-    // pendulum.update(ctx, {
-    //   ...pendulum.state,
-    //   x: frameCountMod,
-    //   theta: (2 * Math.PI * frameCount) / ctx.canvas.width,
-    // });
   };
 
   return <Canvas draw={draw} />;
