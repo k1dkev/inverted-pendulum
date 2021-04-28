68cf9f6930e537c400ec29bb1ce6d53022d4266b
Kevin Kuney <Kevin.Kuney@gmail.com>
2021-04-27 22:55:18 -0500

    good progess on animation

--- hmi/src/App.js
+++ hmi/src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
 import Canvas from "./components/Canvas";
 import Assembly from "./utils/Assembly";
+class Rectangle {
+  #height = 0;
+  #width;
+  constructor(height, width) {
+    this.#height = height;
+    this.#width = width;
+  }
+  // Getter
+  get area() {
+    return this.calcArea();
+  }
+  // Method
+  calcArea() {
+    return this.#height * this.#width;
+  }
+}
 
 function App() {
+  const square = new Rectangle(5, 10);
+  console.log(square.area); // 100
   var assembly = new Assembly(window.innerWidth, window.innerHeight);
   const draw = (ctx, frameCount) => {
-    ctx.canvas.width = assembly.widthPx;
-    ctx.canvas.height = assembly.heightPx;
+    [ctx.canvas.width, ctx.canvas.height] = assembly.canvasSizeUpdate(window.innerWidth, window.innerHeight);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     assembly.draw(ctx);
-    assembly.updateState({
-      x: (frameCount % assembly.widthPx) - assembly.widthPx / 2,
-      theta: frameCount / 100,
-    });
+    assembly.updateState({ x: (((2 * frameCount) / 2) % 500) - 250, theta: frameCount / 100 });
   };
 
   return <Canvas draw={draw} />;
--- hmi/src/utils/Assembly.js
+++ hmi/src/utils/Assembly.js
@@ -1,176 +1,133 @@
 function Assembly(canvasWidth, canvasHeight) {
-  // Dimensions in mm
-  const dims = {
-    // BLOCK
-    BLOCK_WIDTH: 42,
-    BLOCK_HEIGHT: 21,
-    BLOCK_HOLE_LOCATION: 7,
+  // ********* DIMENSIONS *********
+  // BLOCK
+  const BLOCK_W = 42; // WIDTH
+  const BLOCK_H = 21; // HEIGHT
+  const BLOCK_O = 7; // HOLE LOCATION
 
-    // LINEAR_SHAFT
-    LINEAR_SHAFT_DIAMETER: 8,
-    LINEAR_SHAFT_LENGTH: 486.475,
+  // LINEAR SHAFT
+  const LIN_SHAFT_D = 8;
+  const LINEAR_SHAFT_L = 486.475;
 
-    // LINEAR_BEARING
-    LINEAR_BEARING_WIDTH: 30,
-    LINEAR_BEARING_HEIGHT: 22,
-    LINEAR_BEARING_HOLE_LOCATION: 11,
+  // LINEAR BEARING MOUNT
+  // const LINEAR_BEARING_WIDTH = 30;
+  const LIN_BEARING_H = 22;
+  const LIN_BEARING_O = 11;
 
-    // CART_MOUNT_PLATE
-    CART_MOUNT_PLATE_WIDTH: 37,
-    CART_MOUNT_PLATE_HEIGHT: 6.35,
+  // CART MOUNTING PLATE
+  const CART_PLATE_W = 37;
+  const CART_PLATE_H = 6.35;
 
-    // ROTARY_BEARING_MOUNT
-    ROTARY_BEARING_MOUNT_WIDTH: 37,
-    ROTARY_BEARING_MOUNT_HEIGHT: 23,
-    ROTARY_BEARING_MOUNT_RADIUS: 9.525,
-    ROTARY_BEARING_MOUNT_HOLE_LOCATION: 11,
+  // ROTARY BEARING MOUNT
+  // const ROT_BEARING_W = 37;
+  const ROT_BEARING_H = 23;
+  // const ROT_BEARING_R = 9.525;
+  const ROT_BEARING_O = 11;
 
-    // ROTARY_SHAFT
-    ROTARY_SHAFT_DIAMETER: 6.35,
+  // ROTARY_SHAFT
+  // const ROTARY_SHAFT_DIAMETER = 6.35;
 
-    // PENDULUM
-    PENDULUM_HEIGHT: 304.8,
-    PENDULUM_WIDTH: 18.745,
-    PENDULUM_HOLE_LOCATION: 8,
+  // PENDULUM
+  const PEND_H = 304.8;
+  const PEND_W = 18.745;
+  const PEND_O = 8;
+  const PEND_R = 5;
 
-    // STEPPER_MOTOR
-    STEPPER_MOTOR_WIDTH: 42,
-    STEPPER_MOTOR_HEIGHT: 39.3,
+  // STEPPER_MOTOR
+  const MOTOR_W = 42;
+  const MOTOR_H = 39.3;
 
-    // FEET_RISER
-    FEET_RISER_WIDTH: 12.7,
-    FEET_RISER_HEIGHT: 22.525,
+  // MAIN MOUNT
+  const MAIN_MOUNT_W = 570.475;
+  const MAIN_MOUNT_H = 9.525;
 
-    // FEET
-    FEET_DIAMETER: 25.4,
-    FEET_HEIGHT: 7.95,
+  // FEET_RISER
+  // const FEET_RISER_WIDTH = 12.7;
+  // const FEET_RISER_HEIGHT = 22.525;
 
-    // MARGIN
-    MARGIN: 5,
-  };
+  // FEET
+  // const FEET_DIAMETER = 25.4;
+  // const FEET_HEIGHT = 7.95;
+
+  // MARGIN
+  const MARGIN = 5;
 
   // Constructor
-  this.totalWidth =
-    2 *
-      (dims.MARGIN +
-        dims.PENDULUM_HEIGHT -
-        0.5 * dims.CART_MOUNT_PLATE_WIDTH -
-        dims.PENDULUM_HOLE_LOCATION) +
-    dims.LINEAR_SHAFT_LENGTH;
-
-  this.totalHeight =
-    2 * (dims.MARGIN + dims.PENDULUM_HEIGHT - dims.PENDULUM_HOLE_LOCATION);
-
+  const W = 2 * (MARGIN + PEND_H - 0.5 * CART_PLATE_W - PEND_O) + LINEAR_SHAFT_L; // TOTAL WIDTH
+  const H = 2 * (MARGIN + PEND_H - PEND_O); // TOTAL HEIGHT
   this.x = 0;
-  this.theta = Math.PI / 4;
+  this.theta = 0;
+  var pxPerMM = Math.min(canvasWidth / W, canvasHeight / H);
 
-  let pxmmWidth = canvasWidth / this.totalWidth; // pxmm = pixels per mm
-  let pxmmHeight = canvasHeight / this.totalHeight;
-  this.pxmm = pxmmWidth >= pxmmHeight ? pxmmHeight : pxmmWidth;
-  this.widthPx = this.pxmm * this.totalWidth; // unneeded
-  this.heightPx = this.pxmm * this.totalHeight; // unneeded
-
+  // Update canvas size
   this.canvasSizeUpdate = function (canvasWidth, canvasHeight) {
-    let pxmmWidth = canvasWidth / this.totalWidth; // pxmm = pixels per mm
-    let pxmmHeight = canvasHeight / this.totalHeight;
-    this.pxmm = pxmmWidth >= pxmmHeight ? pxmmHeight : pxmmWidth;
+    pxPerMM = Math.min(canvasWidth / W, canvasHeight / H);
+    return [pxPerMM * W, pxPerMM * H];
   };
 
+  // Update state
   this.updateState = function ({ x, theta }) {
     if (x) this.x = x;
     if (theta) this.theta = theta;
+    const MAX_X_VALUE = (LINEAR_SHAFT_L - CART_PLATE_W) / 2.0;
+    this.x = this.x < -MAX_X_VALUE ? -MAX_X_VALUE : this.x > MAX_X_VALUE ? MAX_X_VALUE : this.x;
   };
 
   // Fills a rectangle using mm dimensions and relative to the center
-  this.fillRectRelCenter = function (x, y, w, h) {
-    this.ctx.fillRect(
-      this.pxmm * (this.totalWidth / 2 + x),
-      this.pxmm * (this.totalHeight / 2 + y),
-      this.pxmm * w,
-      this.pxmm * h
-    );
+  CanvasRenderingContext2D.prototype.fillRectRelCenter = function (x, y, w, h) {
+    this.fillRect(pxPerMM * (W / 2 + x), pxPerMM * (H / 2 + y), pxPerMM * w, pxPerMM * h);
   };
 
-  this.drawCart = function () {
-    this.ctx.fillStyle = "Silver";
-    this.fillRectRelCenter(
-      this.x - dims.CART_MOUNT_PLATE_WIDTH / 2,
-      -dims.ROTARY_BEARING_MOUNT_HOLE_LOCATION,
-      dims.CART_MOUNT_PLATE_WIDTH,
-      dims.LINEAR_BEARING_HEIGHT +
-        dims.CART_MOUNT_PLATE_HEIGHT +
-        dims.ROTARY_BEARING_MOUNT_HEIGHT
-    );
+  // creates a rounded rect
+  CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
+    if (w < 2 * r) r = w / 2;
+    if (h < 2 * r) r = h / 2;
+    this.beginPath();
+    this.moveTo(x + r, y);
+    this.arcTo(x + w, y, x + w, y + h, r);
+    this.arcTo(x + w, y + h, x, y + h, r);
+    this.arcTo(x, y + h, x, y, r);
+    this.arcTo(x, y, x + w, y, r);
+    this.closePath();
+    return this;
   };
 
-  this.drawPendulum = function () {
-    this.ctx.fillStyle = "green";
-    this.ctx.translate(
-      this.pxmm * (this.totalWidth / 2 + this.x),
-      this.pxmm * (this.totalHeight / 2)
-    );
-    this.ctx.rotate(this.theta);
-    this.ctx.translate(
-      -this.pxmm * (this.totalWidth / 2 + this.x),
-      -this.pxmm * (this.totalHeight / 2)
-    );
-    this.fillRectRelCenter(
-      this.x - dims.PENDULUM_WIDTH / 2,
-      dims.PENDULUM_HOLE_LOCATION - dims.PENDULUM_HEIGHT,
-      dims.PENDULUM_WIDTH,
-      dims.PENDULUM_HEIGHT
-    );
-  };
-
-  this.drawLeftBlock = function () {
-    this.ctx.fillStyle = "SteelBlue";
-    this.fillRectRelCenter(
-      -dims.LINEAR_SHAFT_LENGTH / 2.0 - dims.BLOCK_WIDTH,
-      dims.ROTARY_BEARING_MOUNT_HEIGHT -
-        dims.ROTARY_BEARING_MOUNT_HOLE_LOCATION +
-        dims.CART_MOUNT_PLATE_HEIGHT +
-        dims.LINEAR_BEARING_HOLE_LOCATION -
-        dims.BLOCK_HOLE_LOCATION,
-      dims.BLOCK_WIDTH,
-      dims.BLOCK_HEIGHT
-    );
-  };
-
-  this.drawRightBlock = function () {
-    this.ctx.fillStyle = "SteelBlue";
-    this.fillRectRelCenter(
-      dims.LINEAR_SHAFT_LENGTH / 2.0,
-      -dims.ROTARY_BEARING_MOUNT_HOLE_LOCATION +
-        dims.ROTARY_BEARING_MOUNT_HEIGHT +
-        dims.CART_MOUNT_PLATE_HEIGHT +
-        dims.LINEAR_BEARING_HOLE_LOCATION -
-        dims.BLOCK_HOLE_LOCATION,
-      dims.BLOCK_WIDTH,
-      dims.BLOCK_HEIGHT
-    );
-  };
-
-  this.drawLinearShaft = function () {
-    this.ctx.fillStyle = "DarkGrey";
-    this.fillRectRelCenter(
-      -dims.LINEAR_SHAFT_LENGTH / 2.0,
-      -dims.ROTARY_BEARING_MOUNT_HOLE_LOCATION +
-        dims.ROTARY_BEARING_MOUNT_HEIGHT +
-        dims.CART_MOUNT_PLATE_HEIGHT +
-        dims.LINEAR_BEARING_HOLE_LOCATION -
-        dims.LINEAR_SHAFT_DIAMETER / 2.0,
-      dims.LINEAR_SHAFT_LENGTH,
-      dims.LINEAR_SHAFT_DIAMETER
-    );
-  };
-
+  // Main draw function
   this.draw = function (ctx) {
-    this.ctx = ctx;
-    this.drawLeftBlock(ctx);
-    this.drawRightBlock(ctx);
-    this.drawLinearShaft(ctx);
-    this.drawCart(ctx);
-    this.drawPendulum(ctx);
+    // draw left block
+    ctx.fillStyle = "SteelBlue";
+    ctx.fillRectRelCenter(-LINEAR_SHAFT_L / 2.0 - BLOCK_W, ROT_BEARING_H - ROT_BEARING_O + CART_PLATE_H + LIN_BEARING_O - BLOCK_O, BLOCK_W, BLOCK_H);
+
+    // draw main plate
+    ctx.fillStyle = "DarkGrey";
+    ctx.fillRectRelCenter(-LINEAR_SHAFT_L / 2.0 - BLOCK_W, ROT_BEARING_H - ROT_BEARING_O + CART_PLATE_H + LIN_BEARING_O - BLOCK_O + BLOCK_H, MAIN_MOUNT_W, MAIN_MOUNT_H);
+
+    // draw right block
+    ctx.fillStyle = "SteelBlue";
+    ctx.fillRectRelCenter(LINEAR_SHAFT_L / 2.0, -ROT_BEARING_O + ROT_BEARING_H + CART_PLATE_H + LIN_BEARING_O - BLOCK_O, BLOCK_W, BLOCK_H);
+
+    // draw stepper motor
+    ctx.fillStyle = "black";
+    ctx.fillRectRelCenter(LINEAR_SHAFT_L / 2.0, -ROT_BEARING_O + ROT_BEARING_H + CART_PLATE_H + LIN_BEARING_O - BLOCK_O - MOTOR_H, MOTOR_W, MOTOR_H);
+
+    // draw linear shaft
+    ctx.fillStyle = "DarkGrey";
+    ctx.fillRectRelCenter(-LINEAR_SHAFT_L / 2.0, -ROT_BEARING_O + ROT_BEARING_H + CART_PLATE_H + LIN_BEARING_O - LIN_SHAFT_D / 2.0, LINEAR_SHAFT_L, LIN_SHAFT_D);
+
+    // draw cart
+    ctx.fillStyle = "Silver";
+    ctx.fillRectRelCenter(this.x - CART_PLATE_W / 2, -ROT_BEARING_O, CART_PLATE_W, LIN_BEARING_H + CART_PLATE_H + ROT_BEARING_H);
+
+    // draw pendulum
+    ctx.fillStyle = "green";
+    ctx.translate(pxPerMM * (W / 2 + this.x), pxPerMM * (H / 2));
+    ctx.rotate(this.theta);
+    ctx.translate(-pxPerMM * (W / 2 + this.x), -pxPerMM * (H / 2));
+    // ctx.fillRectRelCenter(this.x - PEND_W / 2, PEND_O - PEND_H, PEND_W, PEND_H);
+    ctx.roundRect(pxPerMM * (W / 2 + this.x - PEND_W / 2), pxPerMM * (H / 2 + PEND_O - PEND_H), pxPerMM * PEND_W, pxPerMM * PEND_H, pxPerMM * PEND_R).fill();
+    ctx.translate(pxPerMM * (W / 2 + this.x), pxPerMM * (H / 2));
+    ctx.rotate(-this.theta);
+    ctx.translate(-pxPerMM * (W / 2 + this.x), -pxPerMM * (H / 2));
   };
 }
 
